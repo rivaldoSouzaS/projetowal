338770b762e9a71fb266fcd20788c8631abae164
User <rivaldosilva@si.fiponline.edu.br>
2022-03-23 20:58:42 -0300

    post ok

--- backend/index.js
+++ backend/index.js
@@ -9,61 +9,39 @@ async function readExcel(rout){
     const workbook = XLSX.readFile(rout)
     const workbookSheets = workbook.SheetNames;
 
-    //console.log(workbookSheets)
-
     const sheet = workbookSheets[0]
     const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
-
-    //console.log(dataExcel[0].id)
 
     const data = new Date();
     const eficacia = 0;
     validade = true;
+
     
-    /*
-    const novaCampanha = await pool.query(
-        `INSERT INTO cliente(dataCampanha, eficacia, validade) VALUES ($1, $2, $3) RETURNING *`,[data, eficacia, validade]
+    const id = await pool.query(
+        `INSERT INTO campanha(dataCampanha, eficacia, validade) VALUES ($1, $2, $3) RETURNING *`,[data, eficacia, validade]
     )
-    
-    const clientes = await pool.query(
-        `INSERT INTO campanha(idCampanha, idTitulo, nome, vencimento, pago) VALUES ($1, $2, $3, $4, $5) RETURNING *`,[data, eficacia, validade]
-    )
-    */
-    
-    
-    dataExcel.forEach(element => {
-        console.log(element.idCampanha +' '+element.idTitulo, element.nome, element.vencimento, element.pago)
-    });
+
+    //console.log(id.rows[0].id)
+
+    for (let index = 0; index < dataExcel.length; index++) {
+        
+        await inserir(id.rows[0].id, dataExcel[index])
+    }
     
 }
 
-function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
+async function inserir(id, lista){
+    const clientes =  pool.query(
+        `INSERT INTO cliente (idCampanha, idTitulo, nome, vencimento, pago) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+        [id, lista.idTitulo, lista.nome, lista.vencimento, lista.pago]
+    )
+
+    //console.log(id+ " " + lista.idCampanha);
 }
 
-function  formatDate(date) {
-    return [
-      date.getFullYear(),
-      padTo2Digits(date.getMonth() + 1),
-      padTo2Digits(date.getDate()),
-    ].join('-');
-}
 
 app.post('/campanha', async (req, res) =>{
-    try {
-        
-        const data = new Date();
-        const {eficacia} = req.body
-        const {validade} = req.body
-
-        const novaCampanha = await pool.query(
-            `INSERT INTO campanha(dataCampanha, eficacia, validade) VALUES ($1, $2, $3) RETURNING *`,[data, eficacia, validade]
-        )
-        res.json(novaCampanha.rows[0].id)
-        //res.send(novaCampanha.rows.id)
-    } catch (error) {
-        console.log(error);
-    }
+    console.log("blz")
 })
 
 readExcel('baseDados.xlsx')
